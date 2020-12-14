c0d909bdf5688e82dac5170ba0fa61aa1aa8017b
sin <sinvirance@gmail.com>
2020-12-15 04:29:25 +0800

    sinmall init commit

---
+++ src/main/java/top/sinmall/dao/CategoryMapper.java
@@ -0,0 +1,17 @@
+package top.sinmall.dao;
+
+import top.sinmall.pojo.Category;
+
+public interface CategoryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
---
+++ src/main/java/top/sinmall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package top.sinmall.dao;
+
+import top.sinmall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
